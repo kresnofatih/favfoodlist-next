58eaf3366074edc36faa46a0f4edd5a078abb516
Fatih <kresnofatihimani.private@gmail.com>
2021-04-03 22:07:30 +0700

    created reusable comp :ice_cream::shaved_ice:

---
+++ comps/header.js
@@ -0,0 +1,9 @@
+const Header = ({headerTitle}) => {
+    return ( 
+        <h3>
+            {headerTitle}
+        </h3>
+     );
+}
+ 
+export default Header;
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Header from "../comps/header";
+
 const About = () => {
     return (
         <div>
+            <Header headerTitle="about page"/>
             <h1>about</h1>
             <p>lorem ipsum</p>
         </div>
