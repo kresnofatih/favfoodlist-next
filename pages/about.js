b48058c8b94274ee8430827df2b4f95754e70f9b
Fatih <kresnofatihimani.private@gmail.com>
2021-04-04 08:36:27 +0700

    setUp styledcomponents in next project :fire::rocket:

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,16 @@
 import Header from "../comps/header";
+import styled from 'styled-components'
+
+const TitleAbout = styled.h1`
+    font-size: 50px;
+    color: red;
+`
 
 const About = () => {
     return (
         <div>
             <Header headerTitle="about page"/>
+            <TitleAbout>About</TitleAbout>
             <h1>about</h1>
             <p>lorem ipsum</p>
         </div>
